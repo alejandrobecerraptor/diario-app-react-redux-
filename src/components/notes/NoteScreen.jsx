2ac4d1a6d979b06f70b5b10c8a34a5d910fe71e8
Alejandro <alejandro.becerra.pro@gmail.com>
2021-04-16 12:46:57 -0500

    estructura y diseño del home

--- src/components/notes/NoteScreen.jsx
+++ src/components/notes/NoteScreen.jsx
@@ -5,6 +5,25 @@ export const NoteScreen = () => {
   return (
     <div className="notes__main-content">
       <NotesAppBar />
+      <div className="notes__content">
+        <input
+          type="text"
+          placeholder="Some awasome title"
+          className="notes__title-input"
+          autocomplete="off"
+        />
+        <textarea
+          placeholder="What happend today?"
+          className="notes__textarea"
+        ></textarea>
+
+        <div className="notes__image">
+          <img
+            src="https://i0.wp.com/hipertextual.com/wp-content/uploads/2021/04/elon-musk.jpeg?fit=1200%2C835&ssl=1"
+            alt="imagen"
+          />
+        </div>
+      </div>
     </div>
   );
 };
