2dc1c7cd049b5ec844cf8791ba9e356bc4844ead
Alejandro <alejandro.becerra.pro@gmail.com>
2021-04-17 17:45:57 -0500

    mostrar un loading global en la aplicacion

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,4 +1,4 @@
-import React,{useEffect} from "react";
+import React,{useEffect, useState} from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -14,14 +14,28 @@ import { login } from '../actions/auth';
 export const AppRouter = () => {
 
   const dispatch = useDispatch();
+  const [checking, setChecking] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+  
   useEffect(() => {
     firebase.auth().onAuthStateChanged( (user) => {
       if(user?.uid){ // ? el objeto user tiene algo rpegunta el uid
-        dispatch(login(user.uid, user.displayName))
+        dispatch(login(user.uid, user.displayName));
+        setIsLoggedIn(true);
+      }else {
+        setIsLoggedIn(false);
       }
-    });
-  }, [])
 
+      setChecking(false);
+    });
+  }, [dispatch,setChecking,setIsLoggedIn])
+
+  if (checking) {
+    return (
+      <h1>Espere...</h1>
+    )
+  }
 
   return (
     <Router>
