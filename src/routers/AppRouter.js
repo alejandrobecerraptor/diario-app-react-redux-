9b3eb3cea62094c2dd10f2eafa3d4090c4fa89f9
Alejandro <alejandro.becerra.pro@gmail.com>
2021-04-17 17:36:57 -0500

    mantener el estado de autentificacion en el estado

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React,{useEffect} from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
 } from "react-router-dom";
-
+import { useDispatch } from "react-redux";
+import { firebase } from "../firebase/firebase-config";
 import { AuthRouter } from "./AuthRouter";
 import { JournalScreen } from "../components/journal/JournalScreen";
+import { login } from '../actions/auth';
 
 export const AppRouter = () => {
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged( (user) => {
+      if(user?.uid){ // ? el objeto user tiene algo rpegunta el uid
+        dispatch(login(user.uid, user.displayName))
+      }
+    });
+  }, [])
+
+
   return (
     <Router>
       <div>
