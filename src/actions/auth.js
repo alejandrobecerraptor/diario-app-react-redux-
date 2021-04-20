f20188ee6c0cf3ede9feded2d7f7380eb1c6c2eb
Alejandro <alejandro.becerra.pro@gmail.com>
2021-04-20 11:43:00 -0500

    boton logout firebase, redux

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -53,3 +53,14 @@ export const login = (uid, displayName) => ({
     displayName,
   },
 });
+
+export const startLogout = () => {
+  return async (dispatch) => {
+    await firebase.auth().signOut();
+    dispatch(logout());
+  }
+};
+
+export const logout = () => ({ 
+  type: types.logout
+})
--- src/components/journal/Sidebar.jsx
+++ src/components/journal/Sidebar.jsx
@@ -1,21 +1,28 @@
 import React from "react";
-import {JournalEntries} from "./JournalEntries";
+import { JournalEntries } from "./JournalEntries";
+import { useDispatch } from "react-redux";
+import {startLogout} from "../../actions/auth";
 
 export const Sidebar = () => {
+  const dispatch = useDispatch();
+  const handleLogout = () => {
+    dispatch(startLogout());
+  };
+
   return (
     <aside className="journal__sidebar">
       <div className="journal__sidebar-navbar">
         <h3 className="mt-5">
-            <i className="far fa-moon"></i>
+          <i className="far fa-moon"></i>
           <span> Alejandro</span>
         </h3>
-        <button className="btn">
-            Logout
+        <button className="btn" onClick={handleLogout}>
+          Logout
         </button>
       </div>
       <div className="journal__new-entry">
-          <i className="far fa-calendar-plus fa-5x"></i>
-          <p className="mt-5">New Entry</p>
+        <i className="far fa-calendar-plus fa-5x"></i>
+        <p className="mt-5">New Entry</p>
       </div>
 
       <JournalEntries />
