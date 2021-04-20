89d7e1ec81fe6416ab2867d60645567236ace7d4
Alejandro <alejandro.becerra.pro@gmail.com>
2021-04-20 12:03:22 -0500

    proteccion de rutas publicas y privadas

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,49 +1,56 @@
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
   Redirect,
 } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { firebase } from "../firebase/firebase-config";
 import { AuthRouter } from "./AuthRouter";
 import { JournalScreen } from "../components/journal/JournalScreen";
-import { login } from '../actions/auth';
+import { login } from "../actions/auth";
+import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 
 export const AppRouter = () => {
-
   const dispatch = useDispatch();
   const [checking, setChecking] = useState(true);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  
-  
+
   useEffect(() => {
-    firebase.auth().onAuthStateChanged( (user) => {
-      if(user?.uid){ // ? el objeto user tiene algo rpegunta el uid
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user?.uid) {
+        // ? el objeto user tiene algo rpegunta el uid
         dispatch(login(user.uid, user.displayName));
         setIsLoggedIn(true);
-      }else {
+      } else {
         setIsLoggedIn(false);
       }
 
       setChecking(false);
     });
-  }, [dispatch,setChecking,setIsLoggedIn])
+  }, [dispatch, setChecking, setIsLoggedIn]);
 
   if (checking) {
-    return (
-      <h1>Espere...</h1>
-    )
+    return <h1>Espere...</h1>;
   }
 
   return (
     <Router>
       <div>
         <Switch>
-          <Route path="/auth" component={AuthRouter} />
+          <PublicRoute
+            isAuthenticated={isLoggedIn}
+            path="/auth"
+            component={AuthRouter}
+          />
 
-          <Route exact path="/" component={JournalScreen} />
+          <PrivateRoute
+            isAuthenticated={isLoggedIn}
+            exact
+            path="/"
+            component={JournalScreen}
+          />
 
           <Redirect to="/auth/login" />
         </Switch>
